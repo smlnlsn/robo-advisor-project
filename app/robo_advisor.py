4a000f3a0e26d934bb98cc41a65c43f378cc7c4a
smlnlsn <59628711+smlnlsn@users.noreply.github.com>
2020-02-24 23:40:00 -0500

    outputs 1/2 way finished

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -8,7 +8,8 @@ from dotenv import load_dotenv
 import os
 import json
 import requests
-import pandas
+import pandas as pd
+import datetime
 #from alpha_vantage.timeseries import TimeSeries
 
 
@@ -31,6 +32,7 @@ user_input = input("Please enter an IPO: ")
 while user_input != "000": 
     if user_input.isalpha():
         length_check = "Y"
+        stock_data = ""
         if len(user_input) >= 6:
             length_check = input("Your IPO code is pretty long. Are you sure it's correct? (Y/N): ")
         
@@ -41,11 +43,41 @@ while user_input != "000":
             key = os.environ["ALPHAVANTAGE_API_KEY"]
             print("Your key is:", key)
             print("---------------\n")
-
-            request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + user_input + "&interval=5min&outputsize=full&apikey=" + key
+            output_size = "compact"
+            request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + user_input + "&outputsize=" + output_size + "&apikey=" + key
             response = requests.get(request_url)
+            time_of_request = datetime.datetime.now()
+            #print(response.status_code)
             stock_data = json.loads(response.text)
-            print(stock_data['Meta Data'])
+
+            if "Error Message" in stock_data.keys():
+                print("Something went wrong.")
+                print(stock_data["Error Message"])
+            else:
+                df = pd.DataFrame(stock_data)
+                df.to_csv(r"./data/prices_" + user_input.lower() + ".csv")
+
+                last_refreshed = stock_data['Meta Data']['3. Last Refreshed']
+                latest_day = '2020-02-24' #TO-DO: make dynamic
+                latest_close = stock_data['Time Series (Daily)'][latest_day]['4. close']
+
+                #output
+                print("-------------------------")
+                print("SELECTED SYMBOL:", stock_data['Meta Data']['2. Symbol'])
+                print("-------------------------")
+                print("REQUESTING STOCK MARKET DATA...")
+                print("REQUEST AT:", time_of_request.ctime() )
+                print("-------------------------")
+                print("LATEST DAY:", latest_day)
+                print("LATEST CLOSE:", latest_close)
+                print("RECENT HIGH:")
+                print("RECENT LOW:")
+                print("-------------------------")
+                print("RECOMMENDATION:")
+                print("RECOMMENDATION REASON:")
+                print("-------------------------")
+                print("HAPPY INVESTING!")
+                print("-------------------------")
     elif user_input == "000":
         print("Thank you for using the STOCK INFO APP. Goodbye!")
     else:
