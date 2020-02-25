3939c7e849b9be1ff898542b1253328f84b7bec6
smlnlsn <59628711+smlnlsn@users.noreply.github.com>
2020-02-25 10:57:33 -0500

    high/low

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -10,8 +10,17 @@ import json
 import requests
 import pandas as pd
 import datetime
+import ast
 #from alpha_vantage.timeseries import TimeSeries
 
+def find_high(data):
+    return 0
+        
+def find_low(data):
+    return 0
+
+def recommendation(data):
+    return 0
 
 #variables
 user_input = ""
@@ -35,9 +44,9 @@ while user_input != "000":
         stock_data = ""
         if len(user_input) >= 6:
             length_check = input("Your IPO code is pretty long. Are you sure it's correct? (Y/N): ")
-        
         if length_check.upper() == "Y":
             print("You entered:", user_input)
+            user_input = user_input.upper()
             #os.environ["ALPHAVANTAGE_API_KEY"] = input("Please provide your API key: ")
             load_dotenv()
             key = os.environ["ALPHAVANTAGE_API_KEY"]
@@ -47,7 +56,6 @@ while user_input != "000":
             request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + user_input + "&outputsize=" + output_size + "&apikey=" + key
             response = requests.get(request_url)
             time_of_request = datetime.datetime.now()
-            #print(response.status_code)
             stock_data = json.loads(response.text)
 
             if "Error Message" in stock_data.keys():
@@ -55,11 +63,18 @@ while user_input != "000":
                 print(stock_data["Error Message"])
             else:
                 df = pd.DataFrame(stock_data)
-                df.to_csv(r"./data/prices_" + user_input.lower() + ".csv")
-
+                file_path = "./data/prices_" + user_input.lower() + ".csv"
+                df.to_csv(r"%s" % file_path)
+                
                 last_refreshed = stock_data['Meta Data']['3. Last Refreshed']
                 latest_day = '2020-02-24' #TO-DO: make dynamic
                 latest_close = stock_data['Time Series (Daily)'][latest_day]['4. close']
+
+                recent_high = find_high(stock_data)
+                recent_low = find_low(stock_data)
+
+                #recommendation
+                rec = recommendation(stock_data)
 
                 #output
                 print("-------------------------")
@@ -70,14 +85,15 @@ while user_input != "000":
                 print("-------------------------")
                 print("LATEST DAY:", latest_day)
                 print("LATEST CLOSE:", latest_close)
-                print("RECENT HIGH:")
-                print("RECENT LOW:")
+                print("RECENT HIGH:", recent_high)
+                print("RECENT LOW:", recent_low)
                 print("-------------------------")
                 print("RECOMMENDATION:")
                 print("RECOMMENDATION REASON:")
                 print("-------------------------")
                 print("HAPPY INVESTING!")
                 print("-------------------------")
+                
     elif user_input == "000":
         print("Thank you for using the STOCK INFO APP. Goodbye!")
     else:
