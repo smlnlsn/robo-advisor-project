ea8e009b6d226d4dfe19b35a29e172056f7a3d8f
smlnlsn <59628711+smlnlsn@users.noreply.github.com>
2020-02-24 22:44:50 -0500

    Connects to Alpha Vantage
    it can get data from the api!

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -8,11 +8,15 @@ from dotenv import load_dotenv
 import os
 import json
 import requests
-#import pandas
+import pandas
+#from alpha_vantage.timeseries import TimeSeries
+
 
 #variables
 user_input = ""
-api_key = ""
+key = ""
+request_url = ""
+length_check = "Y"
 
 #APP BEGINS
 os.system('clear')
@@ -24,10 +28,26 @@ print("-------------------")
 
 user_input = input("Please enter an IPO: ")
 
-if user_input.isalpha():
-    print("You entered:", user_input)
-    os.environ["ALPHAVANTAGE_API_KEY"] = input("Please provide your API key: ")
-    print("Your key is:", os.environ["ALPHAVANTAGE_API_KEY"])
-else:
-    print("You entered an invalid input. Please try again.")
+while user_input != "000": 
+    if user_input.isalpha():
+        length_check = "Y"
+        if len(user_input) >= 6:
+            length_check = input("Your IPO code is pretty long. Are you sure it's correct? (Y/N): ")
+        
+        if length_check.upper() == "Y":
+            print("You entered:", user_input)
+            #os.environ["ALPHAVANTAGE_API_KEY"] = input("Please provide your API key: ")
+            load_dotenv()
+            key = os.environ["ALPHAVANTAGE_API_KEY"]
+            print("Your key is:", key)
+            print("---------------\n")
 
+            request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + user_input + "&interval=5min&outputsize=full&apikey=" + key
+            response = requests.get(request_url)
+            stock_data = json.loads(response.text)
+            print(stock_data['Meta Data'])
+    elif user_input == "000":
+        print("Thank you for using the STOCK INFO APP. Goodbye!")
+    else:
+        print("You entered an invalid input. Please try again.")
+    user_input = input("Please enter an IPO or \"000\" to quit: ")
